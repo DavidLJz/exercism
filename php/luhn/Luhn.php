66871d44b877e0d9c60537b7d8e831a4cfd0e065
David LJ <4dlj1995@gmail.com>
2021-09-19 12:52:53 -0500

    solved php/luhn

--- php/luhn/Luhn.php
+++ php/luhn/Luhn.php
@@ -1,30 +1,34 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 function isValid(string $number): bool
 {
-    throw new \BadFunctionCallException("Implement the isValid function");
+    $number = trim($number);
+
+    if ( strlen($number) < 2 || preg_match('/[^\d\s]/', $number) ) {
+       return false;
+    }
+
+    $digits = str_split(str_replace(' ', '', $number));
+    $digits = array_reverse($digits);
+
+    $second = false;
+
+    foreach ($digits as $i => $digit) {
+        $digit = (int) $digit;
+
+        if ( $second ) {
+            $digit *= 2;
+
+            if ( $digit > 9 ) {
+                $digit -= 9;
+            }
+        } 
+
+        $digits[$i] = $digit;
+        $second = !$second;
+    }
+
+    return array_sum($digits) % 10 === 0;
 }
